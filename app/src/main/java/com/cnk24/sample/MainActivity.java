05fee5ebe37d928bb8fa88bbdca8ada35635b9e5
ss3589 <41930495+ss3589@users.noreply.github.com>
2018-08-14 16:36:16 +0900

    Initial commit

---
+++ app/src/main/java/com/cnk24/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cnk24.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
